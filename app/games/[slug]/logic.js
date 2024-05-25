4dd7fe25f10aad3a0c28e7de6650a73357fa63d3
Nuno Costa <nuno.mmc@gmail.com>
2024-05-25 19:22:20 +0100

    Fix build error
    Error: Page "/games/[slug]" is missing "generateStaticParams()" so it
    cannot be used with "output: export" config.

--- app/games/[slug]/logic.js
+++
@@ -1,8 +0,0 @@
-export default function LogicTemplate() {
-  return (
-    <div>
-      <h1>Lucky Game Under development</h1>
-      {/* Add the content and structure for the Lucky template */}
-    </div>
-  );
-}
