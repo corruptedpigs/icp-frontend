f367827f6866958951e6bdbc3ea4912d9e4b632a
Nuno Costa <nuno.mmc@gmail.com>
2024-05-28 00:57:56 +0100

    open links on footer in a new tab

--- ui/components/homepage/footer.js
+++ ui/components/homepage/footer.js
@@ -15,11 +15,11 @@ const Footer = () => {
       <nav className="lg:w-1/3 flex flex-col items-center lg:items-end mx-auto">
         <h6 className="footer-title uppercase">Follow the Pigs</h6>
         <div className="flex justify-center items-center gap-4">
-          <Link href="https://x.com/CorruptedPigs" className="text-white">
+          <Link href="https://x.com/CorruptedPigs" className="text-white" target="_blank">
             <Image src="/x-logo.svg" alt="Pig card1" height={40} width={30}/>
           </Link>
           <Link href={discordInviteLink} className="text-white">
-            <Image src="/discord-logo.svg" alt="Pig card1" height={40} width={40}/>
+            <Image src="/discord-logo.svg" alt="Pig card1" target="_blank" height={40} width={40}/>
           </Link>
         </div>
       </nav>
