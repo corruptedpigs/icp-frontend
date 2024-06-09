86b6feeda13713a15a90f1e6071654651f08258f
Nuno Costa <nuno.mmc@gmail.com>
2024-06-09 15:56:05 +0100

    translate associations to english

--- ui/components/homepage/associations.js
+++ ui/components/homepage/associations.js
@@ -15,7 +15,7 @@ const Associations = () => {
             </figure>
             <div className="card-body">
               <h2 className="card-title text-purple">Projeto Rios</h2>
-              <p className="text-clip h-40 overflow-hidden">O Projeto Rios é um projeto ibérico coordenado, a nível nacional, pela Associação Portuguesa de Educação Ambiental (ASPEA) e que incentiva a adoção de troços de 500 metros de rios ou ribeiras por grupos locais organizados. Recorrendo a uma metodologia de observação simples, mas rigorosa, de fácil aplicação e desenvolvimento, os grupos assumem a responsabilidade de vigilância e proteção do troço escolhido, contribuindo assim para a melhoria ambiental dos recursos hídricos, em geral, e para promoção de ações de melhoria do troço, em particular.</p>
+              <p className="text-clip h-40 overflow-hidden">The Projeto Rios is an Iberian initiative coordinated nationally by the Associação Portuguesa de Educação Ambiental (ASPEA). It encourages local groups to adopt 500-meter sections of rivers or streams. Using a simple yet rigorous observation method, these groups take responsibility for monitoring and protecting their chosen section, thereby improving water resources and promoting specific improvements to their section.</p>
               <div className="card-actions justify-end">
                 <Link
                   href="https://enea.apambiente.pt/content/projeto-rios"
@@ -30,11 +30,12 @@ const Associations = () => {
         <div className="carousel-item">
           <div className="card card-compact max-w-80 md:max-w-96 bg-base-100 shadow-xl dark:bg-gray-100 dark:text-black">
             <figure>
-              <Image src="/patav.jpeg" alt="PATAV" width={350} height={300}/>
+              <Image src="/patav.png" alt="PATAV" width={350} height={300}/>
             </figure>
             <div className="card-body">
               <h2 className="card-title text-purple">PATAV</h2>
-              <p className="text-clip h-40 overflow-hidden">PATAV (Plataforma Anti Transporte de Animais Vivos) apresenta-se como um Movimento Cívico que tem como objeto defender a abolição do transporte de animais vivos por via marítima de Portugal para o Médio Oriente e Norte de África.</p>
+              <h3>(Plataforma Anti-Transporte de Animais Vivos)</h3>
+              <p className="text-clip h-40 overflow-hidden">PATAV presents itself as a civic movement whose aim is to defend the abolition of the transport of livestock by sea from Portugal to the Middle East and North Africa.</p>
               <div className="card-actions justify-end">
                 <Link
                   href="https://patav.weebly.com"
@@ -53,7 +54,7 @@ const Associations = () => {
             </figure>
             <div className="card-body">
               <h2 className="card-title text-purple">Federação Nacional dos Baldios</h2>
-              <p className="text-clip h-40 overflow-hidden">A figura do Agrupamento de Baldios, que nunca foi utilizada, revela neste momento uma importância acrescida, uma vez que com a criação de condições específicas de aplicação será dado um passo importante para uma gestão sustentável, com todas as mais-valias que advêm do associativismo e de uma economia de escala.</p>
+              <p className="text-clip h-40 overflow-hidden">BALADI - Federação Nacional dos Baldios, has been working with forestry organisations to establish protocols for management, awareness-raising, fire protection and forest protection.</p>
               <div className="card-actions justify-end">
                 <Link
                   href="https://www.baladi.pt"
