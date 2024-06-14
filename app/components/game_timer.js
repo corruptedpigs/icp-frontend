6c439ffaf064a01dad5e6e599a690691c4bb90bc
Nuno Costa <nuno.mmc@gmail.com>
2024-06-14 23:33:57 +0100

    move glow close to the bg colour

--- app/components/game_timer.js
+++ app/components/game_timer.js
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import { useState, useEffect } from "react";
 
-const GameTimer = () => { 
+const GameTimer = () => {
 
   const [activeHr, setActiveHr] = useState(0);
 
@@ -32,32 +32,32 @@ const GameTimer = () => {
           />
         </div>
 
-        <hr className={`${activeHr > 0 ? '' : "bg-green-600"} glow`}/>
+        <hr className={`${activeHr > 0 ? '' : "bg-green-600 glow"}`}/>
       </li>
       <li>
-        <hr className={`${activeHr > 1 ? '' : "bg-green-500"} glow`}/>
+        <hr className={`${activeHr > 1 ? '' : "bg-green-500 glow"}`}/>
         <div className="timeline-end h-20"></div>
-        <hr className={`${activeHr > 2 ? '' : "bg-green-400"} glow`}/>
+        <hr className={`${activeHr > 2 ? '' : "bg-green-400 glow"}`}/>
       </li>
       <li>
-        <hr className={`${activeHr > 3 ? '' : "bg-amber-500"} glow`}/>
+        <hr className={`${activeHr > 3 ? '' : "bg-amber-500 glow"}`}/>
         <div className="timeline-end h-20"></div>
-        <hr className={`${activeHr > 4 ? '' : "bg-amber-400"} glow`}/>
+        <hr className={`${activeHr > 4 ? '' : "bg-amber-400 glow"}`}/>
       </li>
       <li >
-        <hr className={`${activeHr > 5 ? '' : "bg-orange-500"} glow`}/>
+        <hr className={`${activeHr > 5 ? '' : "bg-orange-500 glow"}`}/>
         <div className="timeline-end h-20"></div>
-        <hr className={`${activeHr > 6 ? '' : "bg-orange-900"} glow`}/>
+        <hr className={`${activeHr > 6 ? '' : "bg-orange-900 glow"}`}/>
       </li>
       <li>
-        <hr className={`${activeHr > 7 ? '' : "bg-red-800"} glow`}/>
+        <hr className={`${activeHr > 7 ? '' : "bg-red-800 glow"}`}/>
         <div className="timeline-end h-20"></div>
-        <hr className={`${activeHr > 8 ? '' : "bg-red-700"} glow`}/>
+        <hr className={`${activeHr > 8 ? '' : "bg-red-700 glow"}`}/>
       </li>
       <li>
-        <hr className={`${activeHr > 9 ? '' : "bg-red-600"} glow`}/>
+        <hr className={`${activeHr > 9 ? '' : "bg-red-600 glow"}`}/>
         <div className="timeline-end h-20"></div>
-        <hr className={`${activeHr > 10 ? '' : "bg-red-500"} glow`}/>
+        <hr className={`${activeHr > 10 ? '' : "bg-red-500 glow"}`}/>
       </li>
     </ul>
   )
