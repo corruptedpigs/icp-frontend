ccc184647266d7a33976cadcb0ede28431567dcf
Nuno Costa <nuno.mmc@gmail.com>
2024-06-15 00:28:09 +0100

    use overflow on gallery

--- app/games/[id]/page.js
+++ app/games/[id]/page.js
@@ -10,23 +10,23 @@ export function generateStaticParams() {
 export default function Page({ params }) {
   return (
     <main className={styles.gameContainer}>
-      <div className="" style={{ backgroundImage: 'url(/background.jpeg)', backgroundSize: "cover", backgroundPosition: 'center bottom' }}>
+      <div style={{ backgroundImage: 'url(/background.jpeg)', backgroundSize: "cover", backgroundPosition: 'center bottom' }}>
         <NavbarWithConnectWallet show_logo={true} />
-        <div className="hero h-screen text-neutral-content">
+        <div className="hero flex-col h-screen text-neutral-content overflow-hidden">
           <div className="hero-overlay bg-opacity-60"></div>
+          <div className="container pt-24 mx-auto h-screen flex">
 
-          <div className="container my-24">
-            <div className="flex gap-4">
-              <div className="flex flex-col">
-                <a className="btn btn-ghost glass no-animation">Gallery</a>
-                <div className="flex flex-col flex-1 overflow-auto gap-6">
-                  <Image src="/images/games/NFTs/FARMER.png" width={200} height={450}></Image>
-                  <Image src="/images/games/NFTs/REFEREE.png" width={200} height={450}></Image>
-                  <Image src="/images/games/NFTs/LAWYER.png" width={200} height={450}></Image>
-                  <Image src="/images/games/NFTs/BANKER.jpg" width={200} height={450}></Image>
-                </div>
+            <div className="flex flex-col">
+              <a className="btn btn-ghost glass no-animation mb-4">Gallery</a>
+              <div class="flex-1/3 flex flex-col hover:overflow-auto gap-4">
+                <Image src="/images/games/NFTs/FARMER.png" width={200} height={450}></Image>
+                <Image src="/images/games/NFTs/REFEREE.png" width={200} height={450}></Image>
+                <Image src="/images/games/NFTs/LAWYER.png" width={200} height={450}></Image>
+                <Image src="/images/games/NFTs/BANKER.jpg" width={200} height={450}></Image>
               </div>
+            </div>
 
+            <div class="flex-1/3 overflow-auto mx-4">
               <div className="flex flex-row gap-4">
                 <div className="flex flex-col">
                   <a className="btn btn-ghost glass no-animation">Player 1</a>
@@ -35,32 +35,34 @@ export default function Page({ params }) {
                   <a className="btn btn-ghost glass no-animation">Your combination</a>
                 </div>
               </div>
+            </div>
 
-              <div className="flex flex-col">
+            <div className="flex flex-col mx-4">
 
-              </div>
+            </div>
 
-
+            <div class="flex-1/3 mx-4">
               <div className="mx-auto">
                 <GameTimer />
               </div>
+            </div>
 
+            <div className="flex flex-col mx-4">
+
+            </div>
+
+            <div className="flex flex-row gap-4 mx-4">
               <div className="flex flex-col">
-
-              </div>
-
-              <div className="flex flex-row gap-4">
-                <div className="flex flex-col">
-                  <a className="btn btn-ghost glass no-animation">Player 2</a>
-                </div>
-                <div className="flex flex-col">
-                  <a className="btn btn-ghost glass no-animation">Your combination</a>
-                </div>
+                <a className="btn btn-ghost glass no-animation">Your combination</a>
               </div>
             </div>
+              <div className="flex flex-col mx-4">
+                <a className="btn btn-ghost glass no-animation">Player 2</a>
+              </div>
           </div>
+
         </div>
       </div>
-    </main>
+    </main >
   );
 }
