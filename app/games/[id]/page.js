205028edcba39267022b2810dcd2ac2982195193
Nuno Costa <nuno.mmc@gmail.com>
2024-06-14 23:29:25 +0100

    change buttons to glass

--- app/games/[id]/page.js
+++ app/games/[id]/page.js
@@ -8,33 +8,31 @@ export function generateStaticParams() {
 }
 
 export default function Page({ params }) {
-  const { id } = params
-  console.debug(id);
-
   return (
     <main className={styles.gameContainer}>
       <div className="" style={{ backgroundImage: 'url(/background.jpeg)', backgroundSize: "cover", backgroundPosition: 'center bottom' }}>
         <NavbarWithConnectWallet show_logo={true} />
         <div className="hero h-screen text-neutral-content">
           <div className="hero-overlay bg-opacity-60"></div>
-          { /* <h1 className="text-3xl">Logic</h1> */ }
 
           <div className="container my-24">
             <div className="flex gap-4">
-              <div className="flex flex-col gap-6 max-h-screen hover:overflow-scroll">
-                <a className="btn btn-warning btn-sm">Gallery</a>
-                <Image src="/images/games/NFTs/FARMER.png" width={200} height={450}></Image>
-                <Image src="/images/games/NFTs/REFEREE.png" width={200} height={450}></Image>
-                <Image src="/images/games/NFTs/LAWYER.png" width={200} height={450}></Image>
-                <Image src="/images/games/NFTs/BANKER.jpg" width={200} height={450}></Image>
+              <div className="flex flex-col">
+                <a className="btn btn-ghost glass no-animation">Gallery</a>
+                <div className="flex flex-col flex-1 overflow-auto gap-6">
+                  <Image src="/images/games/NFTs/FARMER.png" width={200} height={450}></Image>
+                  <Image src="/images/games/NFTs/REFEREE.png" width={200} height={450}></Image>
+                  <Image src="/images/games/NFTs/LAWYER.png" width={200} height={450}></Image>
+                  <Image src="/images/games/NFTs/BANKER.jpg" width={200} height={450}></Image>
+                </div>
               </div>
 
               <div className="flex flex-row gap-4">
                 <div className="flex flex-col">
-                  <a className="btn btn-warning text-xl">Player 1</a>
+                  <a className="btn btn-ghost glass no-animation">Player 1</a>
                 </div>
                 <div className="flex flex-col">
-                  <a className="btn btn-warning text-xl">Your combination</a>
+                  <a className="btn btn-ghost glass no-animation">Your combination</a>
                 </div>
               </div>
 
@@ -53,10 +51,10 @@ export default function Page({ params }) {
 
               <div className="flex flex-row gap-4">
                 <div className="flex flex-col">
-                  <a className="btn btn-warning text-xl">Player 2</a>
+                  <a className="btn btn-ghost glass no-animation">Player 2</a>
                 </div>
                 <div className="flex flex-col">
-                  <a className="btn btn-warning text-xl">Your combination</a>
+                  <a className="btn btn-ghost glass no-animation">Your combination</a>
                 </div>
               </div>
             </div>
