c552751819740cd4497b7cb827d0f3cbae820550
Nuno Costa <nuno.mmc@gmail.com>
2024-06-15 22:36:46 +0100

    update modal to use DaisyUi class

--- app/components/games/modalTimeIsUp.js
+++ app/components/games/modalTimeIsUp.js
@@ -1,26 +1,27 @@
 "use client";
 
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 const ModalTimeIsUp = ({ isOpen, onClose, message }) => {
+  const modalCheckboxRef = useRef(null);
+
   useEffect(() => {
-    if (isOpen) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = 'unset';
+    if (modalCheckboxRef.current) {
+      modalCheckboxRef.current.checked = isOpen;
     }
   }, [isOpen]);
 
-  if (!isOpen) return null;
-
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white rounded-lg p-8">
-        <h2 className="text-2xl mb-4">{message}</h2>
-        <div className="text-4xl">⌛</div> {/* Emoticon */}
-        <button onClick={onClose} className="btn btn-primary mt-4">Close</button>
+    <>
+      <input type="checkbox" id="my_modal_7" className="modal-toggle" ref={modalCheckboxRef} />
+      <div className="modal " role="dialog">
+        <div className="modal-box w-3/12 bg-slate-700 text-center">
+          <h3 className="text-lg font-bold">{message}</h3>
+          <p className="text-4xl py-4">⌛</p>
+        </div>
+        <label className="modal-backdrop" htmlFor="my_modal_7" onClick={onClose}>Close</label>
       </div>
-    </div>
+    </>
   );
 };
 
