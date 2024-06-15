6ac3dbc0718ea7e9b3e29eb4db22d7915bce84bf
Henrique Costa <hmcosta2002@gmail.com>
2024-06-15 01:18:12 +0100

    simplify flex container

--- app/games/[id]/page.js
+++ app/games/[id]/page.js
@@ -1,6 +1,5 @@
 import NavbarWithConnectWallet from "../../../ui/components/homepage/navbar_with_connect_wallet";
 import GameTimer from "../../components/game_timer";
-import styles from '../../../styles/Game.module.css';
 import Image from "next/image";
 
 export function generateStaticParams() {
@@ -16,13 +15,14 @@ export default function Page({ params }) {
   ];
 
   return (
-    <main className={styles.gameContainer}>
-      <div style={{ backgroundImage: 'url(/background.jpeg)', backgroundSize: "cover", backgroundPosition: 'center bottom' }}>
+    <main>
+      <div style={{ backgroundImage: 'url(/background.jpeg)', backgroundSize: "cover", backgroundPosition: 'center bottom' }} className="flex justify-center">
         <NavbarWithConnectWallet show_logo={true} />
-        <div className="hero flex-col h-screen text-neutral-content overflow-hidden">
-          <div className="hero-overlay bg-opacity-60"></div>
-          <div className="container pt-24 mx-auto h-screen flex">
-
+        <div className="hero hero-overlay h-screen text-neutral-content overflow-hidden flex justify-around">
+          
+          {/* Player 1 Game */}
+          <div className="pt-24 h-screen flex gap-4"> 
+            
             <div className="flex flex-col">
               <a className="btn btn-ghost glass no-animation mb-4">Gallery</a>
               <div class="flex-1/3 flex flex-col hover:overflow-auto gap-4 scrollbar-hide">
@@ -38,37 +38,25 @@ export default function Page({ params }) {
               </div>
             </div>
 
-            <div class="flex-1/3 overflow-auto mx-4">
-              <div className="flex flex-row gap-4">
-                <div className="flex flex-col">
-                  <a className="btn btn-ghost glass no-animation">Player 1</a>
-                </div>
-                <div className="flex flex-col">
-                  <a className="btn btn-ghost glass no-animation">Your combination</a>
-                </div>
-              </div>
+            <div className="flex flex-col">
+              <a className="btn btn-ghost glass no-animation">Player 1</a>
             </div>
-
-            <div className="flex flex-col mx-4">
-
+            <div className="flex flex-col">
+              <a className="btn btn-ghost glass no-animation">Your combination</a>
             </div>
-
-            <div class="flex-1/3 mx-4">
-              <div className="mx-auto">
-                <GameTimer />
-              </div>
-            </div>
-
-            <div className="flex flex-col mx-4">
-
-            </div>
-
-            <div className="flex flex-row gap-4 mx-4">
+          </div>
+          
+          {/* Game timer */}
+          <div className="flex">
+            <GameTimer />
+          </div>
+          
+          {/* Player 2 Game */}
+          <div className="pt-24 h-screen flex gap-4">
               <div className="flex flex-col">
                 <a className="btn btn-ghost glass no-animation">Your combination</a>
               </div>
-            </div>
-            <div className="flex flex-col mx-4">
+            <div className="flex flex-col">
               <a className="btn btn-ghost glass no-animation">Player 2</a>
             </div>
           </div>
